7315963ca62529203d094e65a8e0044fd2340766
Miguel-ADS-86 <miguelSistenas.emp@gmail.com>
2022-07-23 10:49:00 -0300

    finalização do estudo com OO

---
+++ src/modulo_17/Sistema_Desktop.java
@@ -0,0 +1,8 @@
+package modulo_17;
+
+public class Sistema_Desktop {/*
+
+Nesse modulo foi falado de camadas e de web e desktop
+finalizando aqui toda a parte de OO.
+proximos códigos vai ser com outros projetos e comentarios dos mesmos. : ) 
+*/}
